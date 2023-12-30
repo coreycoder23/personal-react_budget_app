7cfa0305fc759f1383cc8eabf5b2e52da23cd119
Corey.dev_ <corey.coder23@gmail.com>
2023-12-30 01:17:24 -0500

    Creating currency file and node_module update

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining,currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -57,8 +58,12 @@ const AllocationForm = (props) => {
                   <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
+                
+                <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
+                  </div>
                   </select>
-
+                  <span style={{ "position":"relative","right":"-20px"}}> {currency} </span>
                     <input
                         required='required'
                         type='number'
@@ -67,7 +72,7 @@ const AllocationForm = (props) => {
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
-
+                
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
